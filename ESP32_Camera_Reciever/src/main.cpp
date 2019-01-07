50de54aca831c68f2892223e5e6d46673ad338a3
Calin <botofancalin@gmail.com>
2019-01-07 22:39:09 +0200

    Added Loboris TFT library for better fps

--- ESP32_Camera_Reciever/src/main.cpp
+++ ESP32_Camera_Reciever/src/main.cpp
@@ -1,13 +1,22 @@
-#include "Adafruit_GFX.h"
-#include "WROVER_KIT_LCD.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-/*Uncomment the following line to get the read and display timings*/
-#define TIMINGS
+#ifdef USE_DMA
+extern "C"
+{
+#include "tftspi.h"
+#include "tft.h"
+}
+#else
+#include "Adafruit_GFX.h"
+#include "WROVER_KIT_LCD.h"
 
-//Objects
 WROVER_KIT_LCD tft;
+#endif
+
+/*Uncomment the following line to get the read and display timings*/
+//#define TIMINGS
+
 HTTPClient http;
 
 //Variables
@@ -29,7 +38,9 @@ void reconnect()
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
+#ifndef USE_DMA
     tft.print(".");
+#endif
     delay(1000);
   }
 }
@@ -37,11 +48,77 @@ void reconnect()
 void setup()
 {
   Serial.begin(115200);
+
+#ifdef USE_DMA
+  esp_err_t ret;
+
+  tft_disp_type = DISP_TYPE_ILI9341;
+  _width = DEFAULT_TFT_DISPLAY_WIDTH;  // smaller dimension
+  _height = DEFAULT_TFT_DISPLAY_HEIGHT; // larger dimension
+  TFT_PinsInit();
+
+  spi_lobo_device_handle_t spi;
+
+  spi_lobo_bus_config_t buscfg;
+  buscfg.mosi_io_num = PIN_NUM_MOSI; // set SPI MOSI pin
+  buscfg.miso_io_num = PIN_NUM_MISO; // set SPI MISO pin
+  buscfg.sclk_io_num = PIN_NUM_CLK;  // set SPI CLK pin
+  buscfg.quadwp_io_num = -1;
+  buscfg.quadhd_io_num = -1;
+  buscfg.max_transfer_sz = 6 * 1024;
+
+  spi_lobo_device_interface_config_t devcfg;
+  devcfg.mode = 0;                         // SPI mode 0
+  devcfg.clock_speed_hz = 8000000;         // Initial clock out at 8 MHz
+  devcfg.spics_io_num = -1;                // we will use external CS pin
+  devcfg.spics_ext_io_num = PIN_NUM_CS;    // external CS pin
+  devcfg.flags = LB_SPI_DEVICE_HALFDUPLEX; // ALWAYS SET  to HALF DUPLEX MODE!! for display spi
+
+  vTaskDelay(500 / portTICK_RATE_MS);
+  printf("\r\n==============================\r\n");
+  printf("Pins used: miso=%d, mosi=%d, sck=%d, cs=%d\r\n", PIN_NUM_MISO, PIN_NUM_MOSI, PIN_NUM_CLK, PIN_NUM_CS);
+  printf("==============================\r\n\r\n");
+
+  // ==================================================================
+  // ==== Initialize the SPI bus and attach the LCD to the SPI bus ====
+
+  ret = spi_lobo_bus_add_device(SPI_BUS, &buscfg, &devcfg, &spi);
+  assert(ret == ESP_OK);
+  printf("SPI: display device added to spi bus (%d)\r\n", SPI_BUS);
+  disp_spi = spi;
+
+  // ==== Test select/deselect ====
+  ret = spi_lobo_device_select(spi, 1);
+  assert(ret == ESP_OK);
+  ret = spi_lobo_device_deselect(spi);
+  assert(ret == ESP_OK);
+
+  printf("SPI: attached display device, speed=%u\r\n", spi_lobo_get_speed(spi));
+  printf("SPI: bus uses native pins: %s\r\n", spi_lobo_uses_native_pins(spi) ? "true" : "false");
+
+  printf("SPI: display init...\r\n");
+  TFT_display_init();
+  printf("OK\r\n");
+
+  // ---- Detect maximum read speed ----
+  max_rdclock = find_rd_speed();
+  printf("SPI: Max rd speed = %u\r\n", max_rdclock);
+
+  // ==== Set SPI clock used for display operations ====
+  spi_lobo_set_speed(spi, DEFAULT_SPI_CLOCK);
+  printf("SPI: Changed speed to %u\r\n", spi_lobo_get_speed(spi));
+
+  TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
+  TFT_setRotation(LANDSCAPE);
+  TFT_setFont(DEFAULT_FONT, NULL);
+  TFT_resetclipwin();
+#else
   tft.begin();
-  #ifdef MYDEV
-  tft.setRotation(1);
-  #endif
   tft.fillScreen(0);
+#ifdef MYDEV
+  tft.setRotation(1);
+#endif
+#endif
   reconnect();
 }
 
@@ -49,8 +126,10 @@ void loop()
 {
   if (WiFi.status() != WL_CONNECTED)
   {
+#ifndef USE_DMA
     tft.fillScreen(0);
     tft.print("Reconnecting...");
+#endif
     delay(1000);
     reconnect();
   }
@@ -112,7 +191,11 @@ void loop()
           Serial.printf("read toke %d\n", (b - a));
           b = millis();
 #endif
+#ifdef USE_DMA
+          TFT_jpg_image(CENTER, CENTER, 0, NULL, buff, len);
+#else
           tft.drawJpg(buff, len);
+#endif
 #ifdef TIMINGS
           d = millis();
           Serial.printf("display toke %d\n", (d - b));
@@ -121,6 +204,6 @@ void loop()
         }
       }
     }
-    http.end();
   }
+  http.end();
 }
